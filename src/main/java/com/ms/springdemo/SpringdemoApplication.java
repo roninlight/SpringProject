61f9640ccedcf9c0da7eff5f2e01a4bdf313c098
Monesh Singh <singhmonesh101@gmail.com>
2025-09-06 03:03:25 +0530

    New Spring project

---
+++ src/main/java/com/ms/springdemo/SpringdemoApplication.java
@@ -0,0 +1,15 @@
+package com.ms.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringdemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext app = SpringApplication.run(SpringdemoApplication.class, args);
+
+	}
+
+}
